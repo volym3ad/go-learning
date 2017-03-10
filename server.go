e21c5ca8314f0a11a72306c2dfe53f96bec507d2
Vladyslav Voloshyn <v.voloshyn@mobilunity.com>
2017-03-10 13:02:08 +0200

    first commit

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/go-martini/martini"
+
+func main() {
+
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello world"
+	})
+
+	m.Get("/hello/:name", func(params martini.Params) string {
+		return "Hello " + params["name"]
+		})
+	m.Run()
+}
